4a796c63873040774926d56dbfab01fe1353fdd1
zhangxu <zx73807@gmail,com>
2018-04-20 22:20:51 +0800

    完成json类的构建

---
+++ app/src/main/java/android/enjoyweather/com/enjoyweather/gson/AQI.java
@@ -0,0 +1,9 @@
+package android.enjoyweather.com.enjoyweather.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
